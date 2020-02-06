6d83ba5402070024a5602259d4ba2163b3de4a13
Jamie Saxon <jsaxon@uchicago.edu>
2020-02-06 10:05:59 -0600

    crayons

--- scripts/finding_colored_balls.py
+++ scripts/finding_colored_balls.py
@@ -4,23 +4,193 @@ import numpy as np
 import cv2 
 import glob
 
-import imutils
+#import imutils
 
 from time import sleep, time
 
 import re, sys
 
-ball_colors_bgr = {"red" : (35, 35, 160),
-                   "circles" : (35, 0, 150),
-                   "yellow" : (48, 145, 170), 
-                   "green" : (40, 100, 40),
-                   "hello" : (45, 105, 35),
+ball_colors_bgr = {"red" : (20, 45, 180),
+                   "orange" : (30, 80, 190),
+                   "yellow" : (0, 190, 190), 
+                   "green" : (50, 135, 35),
                    "blue" : (130, 30, 15),
-                   "magenta" : (90, 45, 155)}
+                   "magenta" : (60, 45, 180)}
+
+ball_colors_lab = {k : cv2.cvtColor(np.array(v, dtype="uint8").reshape(1, 1, 3), 
+                                    cv2.COLOR_RGB2LAB)[0,0,:].astype(int)
+                   for k, v in ball_colors_bgr.items()}
+
+
+def get_ball_color_mog(roi, bkd_color, blur = 5, thresh = 20):
+
+    mask = bkd_color.apply(roi, learningRate = 0)
+    mask = cv2.erode(mask, None, iterations = 2)
+
+    roi_lab = cv2.cvtColor(roi, cv2.COLOR_RGB2LAB)
+
+    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return cv2.bitwise_or(roi, roi, mask = mask)
+
+
+    balls = []
+    detections = []
+    for c in contours:
+
+        area = cv2.contourArea(c)
+
+        # print(color, area)
+        if area < 25:  continue
+        if area > 800: continue
+
+        for color, lab in ball_colors_lab.items():
+
+            mask = np.zeros(roi.shape[:2], np.uint8)
+            cv2.drawContours(mask, [c], -1, 255, -1)
+
+            mask3d = np.broadcast_to(np.expand_dims(mask > 0, axis = 2), roi_lab.shape)
+            mean_color = roi_lab[mask > 0].mean(axis = 0)
+            # print(mean_color)
+
+            color_distance = np.sqrt(np.sum(np.subtract(mean_color, lab.astype(int))**2))
+
+            if color == "yellow" and color_distance > 15: continue
+
+            detections.append([color, mean_color, color_distance, area])
+
+
+    if len(detections):
+
+        # print("==========================")
+        # for d in detections: print(d)
+
+        ## Smallest color distance...
+        detections.sort(key = lambda x: x[2])
+        detection = detections[0]
+        
+        lab_color = np.array(detection[1], dtype="uint8").reshape(1, 1, 3)
+        bgr_color = cv2.cvtColor(lab_color, cv2.COLOR_LAB2RGB)[0,0,:]
+        bgr_color = tuple(bgr_color.ravel())
+        
+        print(detection, bgr_color)
+        # print(detection[0], end = " ", flush = True)
+
+        return bgr_color
+
+    return None
+
+
+ 
+def get_ball_color(img, xy, size = 80, blur = 5, thresh = 20):
+
+    y, x = xy
+
+    roi = img[x-size//2:x+size//2, y-size//2:y+size//2]
+
+    detections = []
+    for color, value in ball_colors_bgr.items():
+
+        lab = np.array(value, dtype="uint8").reshape(1, 1, 3)
+        lab = cv2.cvtColor(lab, cv2.COLOR_RGB2LAB)[0,0,:].astype(int)
+
+        img_blur = cv2.GaussianBlur(roi, (blur, blur), 0)
+        img_lab = cv2.cvtColor(img_blur, cv2.COLOR_RGB2LAB)
+
+        lab_diff = np.subtract(img_lab, lab.astype(int))
+        lab_diff = np.sqrt(np.sum(lab_diff ** 2, axis = 2))
+        
+        mask = np.array((lab_diff < thresh) * 255, dtype = np.uint8)
+        mask = cv2.dilate(mask, None, iterations = 2)
+        mask = cv2.erode(mask, None, iterations = 2)
+
+        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        balls = []
+        for c in contours:
+
+            area = cv2.contourArea(c)
+
+            # print(color, area)
+            if area < 50:  continue
+            if area > 700: continue
+
+            mask = np.zeros(roi.shape[:2], np.uint8)
+            cv2.drawContours(mask, [c], -1, 255, -1)
+
+            mask3d = np.broadcast_to(np.expand_dims(mask > 0, axis = 2), img_lab.shape)
+            mean_color = img_lab[mask > 0].mean(axis = 0)
+            # print(mean_color)
+
+            color_distance = np.sqrt(np.sum(np.subtract(mean_color, lab.astype(int))**2))
+
+            if color == "yellow" and color_distance > 15: continue
+
+            detections.append([color, mean_color, color_distance, area])
+
+
+    if len(detections):
+
+        # print("==========================")
+        # for d in detections: print(d)
+
+        ## Smallest color distance...
+        detections.sort(key = lambda x: x[2])
+        detection = detections[0]
+        
+        lab_color = np.array(detection[1], dtype="uint8").reshape(1, 1, 3)
+        bgr_color = cv2.cvtColor(lab_color, cv2.COLOR_LAB2RGB)[0,0,:]
+        bgr_color = tuple(bgr_color.ravel())
+        
+        # print(detection, bgr_color)
+        print(detection[0], end = " ", flush = True)
+
+        return bgr_color
+
+    return None
+
+
+def get_ball_positions(img, color = "red", blur = 3, thresh = 50):
+
+    if type(color) is str:
+        color = ball_colors_bgr[color]
+    
+    # print(color, ball_colors_bgr["magenta"])
+
+    lab = np.array(color, dtype="uint8").reshape(1, 1, 3)
+    lab = cv2.cvtColor(lab, cv2.COLOR_RGB2LAB)[0,0,:].astype(int)
+
+    img_blur = cv2.GaussianBlur(img, (blur, blur), 0)
+    img_lab = cv2.cvtColor(img_blur, cv2.COLOR_RGB2LAB)
+
+    lab_diff = np.subtract(img_lab, lab.astype(int))
+    lab_diff = np.sqrt(np.sum(lab_diff ** 2, axis = 2))
+    
+    mask = np.array((lab_diff < thresh) * 255, dtype = np.uint8)
+
+    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    balls = []
+    for c in contours:
+
+        area = cv2.contourArea(c)
+        if area > 150: continue
+        if area < 4:   continue
+
+        M = cv2.moments(c)
+
+        if M["m00"]: 
+            balls.append([M["m10"] / M["m00"],
+                          M["m01"] / M["m00"]])
+
+    return np.array(balls).astype(int)
+
+
 
 def find_balls_in_image(fname, color = "red", thresh = 0.2, debug = False):
 
     if debug: print(fname, color, thresh, debug)
+
     lab = np.array(ball_colors_bgr[color], dtype="uint8").reshape(1, 1, 3)
     lab = cv2.cvtColor(lab, cv2.COLOR_RGB2LAB)[0,0,:].astype(int)
 
