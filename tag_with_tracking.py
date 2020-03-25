b10ef55d68d872a3f82b119eb925c0dd9f3d56d0
akwhaley <amandakristenwhaley@gmail.com>
2020-03-25 18:27:26 -0500

    incorporate cv multitracking with tagging

---
+++ tag_with_tracking.py
@@ -0,0 +1,182 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+from object_detection.utils import dataset_util
+import os
+from PIL import Image
+from random import sample
+from random import randint
+import argparse
+
+import train
+
+
+def createTrackerByName(trackerType):
+    trackerTypes = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    # Create a tracker based on tracker name
+    if trackerType == trackerTypes[0]:
+        tracker = cv2.TrackerBoosting_create()
+    elif trackerType == trackerTypes[1]:
+        tracker = cv2.TrackerMIL_create()
+    elif trackerType == trackerTypes[2]:
+        tracker = cv2.TrackerKCF_create()
+    elif trackerType == trackerTypes[3]:
+        tracker = cv2.TrackerTLD_create()
+    elif trackerType == trackerTypes[4]:
+        tracker = cv2.TrackerMedianFlow_create()
+    elif trackerType == trackerTypes[5]:
+        tracker = cv2.TrackerGOTURN_create()
+    elif trackerType == trackerTypes[6]:
+        tracker = cv2.TrackerMOSSE_create()
+    elif trackerType == trackerTypes[7]:
+        tracker = cv2.TrackerCSRT_create()
+    else:
+        tracker = None
+        print('Incorrect tracker name')
+        print('Available trackers are:')
+        for t in trackerTypes:
+            print(t)
+
+    return tracker
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video', required=True, help='Path of the video file.')
+    parser.add_argument('--records', default=20, help='Number of frames to tag.', type=int)
+    parser.add_argument('--duration', help='Duration of the video in minutes.', required=True, type=float)
+    parser.add_argument('--width', help='Screen width in pixels.', type = int, required=True)
+    parser.add_argument('--height', help='Screen height in pixels.', type = int, required=True)
+    parser.add_argument("--save", default = False, help='Boolean: save tagged images')
+    parser.add_argument("--json", default = False, help='Boolean: Write results out to json')
+
+    args = parser.parse_args()
+
+    num_records = args.records
+    save_tagged_image = args.save
+    write_to_json = args.json
+    videoFile = args.video
+    video_duration = args.duration*60.
+    screen_width = args.width
+    screen_height = args.height
+
+    screen_dim = (screen_width, screen_height)
+
+    # Specify the tracker type
+    trackerType = "CSRT"
+
+    #Get prior frames
+    #Make frame set
+    frames_tagged, output_path = train.get_priors(videoFile)
+    print("frames_tagged: ", frames_tagged)
+    print("output_path: ", output_path)
+
+    #Make frames to tag list
+    cap = cv2.VideoCapture(videoFile)
+    frameRate = cap.get(5) #frame rate
+    try:
+        frame_set = set(sample(range(int(video_duration*frameRate)), num_records))
+    except ValueError:
+        print("Sample larger than population or is negative - \
+                Choose a different number of frames to tag.")
+
+    #Initialize writer
+    writer = tf.io.TFRecordWriter(output_path)
+
+    records_count = 0
+    num_tagged = 0
+    time_tagging = 0
+    frameId = -1
+
+    #Grab first frame - check if it's in the frame set
+    try:
+        while(cap.isOpened() and num_tagged < num_records):
+            frameId = cap.get(1) #current frame number
+            ret, image = cap.read()
+            if not ret:
+                print("Did not read frame")
+                break
+
+            if int(frameId) in frame_set and int(frameId) not in frames_tagged:
+                print("Frame number {}.  This is record {} out of {}.".format(
+                                frameId, num_tagged + 1, num_records))
+
+                #Tag all objects in frame - save info as output_dict
+                image = cv2.resize(image, screen_dim, interpolation = cv2.INTER_AREA)
+                output_dict, frame_time = train.tag_objects(frameId, image, videoFile)
+
+                #Write out to tfrecord
+                tf_example = train.create_tf_example(output_dict)
+                writer.write(tf_example.SerializeToString())
+                records_count += 1
+                num_tagged += 1
+                time_tagging += frame_time
+                print("Running tag rate: {} seconds per frame".format(
+                        time_tagging/records_count))
+
+                #Set tracker bboxes as list of tagged objects
+                bboxes = []
+                colors = []
+                for i in range(len(output_dict['classes'])):
+                    bboxes.append((output_dict['xmins'][i],
+                                   output_dict['ymins'][i],
+                                   output_dict['xmaxs'][i] - output_dict['xmins'][i],
+                                   output_dict['ymaxs'][i] - output_dict['ymins'][i]))
+                    colors.append((randint(0, 255), randint(0, 255), randint(0, 255)))
+
+                #Create new multitracker object
+                multiTracker = cv2.MultiTracker_create()
+
+                #Add bboxes to tracker object
+                for bbox in bboxes:
+                    multiTracker.add(createTrackerByName(trackerType), image, bbox)
+
+                #Read next frame and update multitracker object
+                #Display bboxes (on cloned image) and ask user if they accept the frame or not.  If not quit
+                while True:
+                    next_frameId = cap.get(1)
+                    ret, next_image = cap.read()
+                    next_image = cv2.resize(next_image, screen_dim, interpolation = cv2.INTER_AREA)
+                    clone = next_image.copy()
+                    if not ret:
+                        print("Could not read frame.")
+                        break
+                    ret, boxes = multiTracker.update(next_image)
+
+                    # draw tracked objects
+                    for i, newbox in enumerate(boxes):
+                        p1 = (int(newbox[0]), int(newbox[1]))
+                        p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
+                        cv2.rectangle(clone, p1, p2, colors[i], 2, 1)
+
+                    # show frame
+                    cv2.namedWindow("MultiTracker")
+                    cv2.startWindowThread()
+                    cv2.imshow('MultiTracker', clone)
+                    cv2.waitKey(100)
+                    print("Press y to accept this tagged frame.")
+                    print("Press any other key to quit without accepting.")
+                    k = cv2.waitKey(0) & 0xFF
+                    if (k == 121):  # y is pressed
+                        next_output_dict = train.update_tracked(output_dict, boxes, next_image, next_frameId)
+                        next_tf_example = train.create_tf_example(next_output_dict)
+                        writer.write(next_tf_example.SerializeToString())
+                        records_count += 1
+                    else:
+                        cv2.destroyWindow('MultiTracker')
+                        cv2.waitKey(100)
+                        break
+
+    except KeyboardInterrupt:
+        print("Tagging interrupted.  Writing out {} records instead of {}.".format(
+                num_tagged, num_records))
+
+    cap.release()
+    cv2.waitKey(10)
+    cv2.destroyAllWindows()
+    cv2.waitKey(10)
+    writer.close()
+    train.print_summary(records_count, output_path)
+
+if __name__ == '__main__': main()
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
-import os
+import os, time
 import tensorflow as tf
 import numpy as np
 from object_detection.utils import dataset_util
+import cv2
 
 
 def print_tagging_instructions():
@@ -27,6 +28,7 @@ def print_summary(num_records, output_path):
     print("*******************************************************************")
 
 def create_tf_example(example):
+    video = str.encode(example['video'])
     source_id = str.encode(example['source_id'])
     height = example['image_height']
     width = example['image_width']
@@ -45,6 +47,7 @@ def create_tf_example(example):
         classes_text.append(label.encode('utf-8')) # List of string class name of bounding box (1 per box)
     classes = example['classes'] # List of integer class id of bounding box (1 per box)
     tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/video': dataset_util.bytes_feature(video),
         'image/height': dataset_util.int64_feature(height),
         'image/width': dataset_util.int64_feature(width),
         'image/source_id': dataset_util.bytes_feature(source_id),
@@ -61,6 +64,7 @@ def create_tf_example(example):
 
 def read_tfrecord(serialized_example):
     feature_description = {
+        'image/video': tf.io.FixedLenFeature([], tf.string),
         'image/height': tf.io.FixedLenFeature([], tf.int64),
         'image/width': tf.io.FixedLenFeature([], tf.int64),
         'image/source_id': tf.io.FixedLenFeature([], tf.string),
@@ -74,6 +78,7 @@ def read_tfrecord(serialized_example):
         'image/object/class/text': tf.io.FixedLenSequenceFeature([], dtype=tf.string, allow_missing=True)
     }
     example = tf.io.parse_single_example(serialized_example, feature_description)
+    image_video = example['image/video']
     image_height = example['image/height']
     image_width = example['image/width']
     image_source_id = example['image/source_id']
@@ -91,6 +96,7 @@ def read_tfrecord(serialized_example):
 
 def read_tfrecord_framenumber(serialized_example):
     feature_description = {
+        'image/video': tf.io.FixedLenFeature([], tf.string),
         'image/height': tf.io.FixedLenFeature([], tf.int64),
         'image/width': tf.io.FixedLenFeature([], tf.int64),
         'image/source_id': tf.io.FixedLenFeature([], tf.string),
@@ -122,12 +128,139 @@ def get_priors(videoFile):
         vol += 1
         output_path = os.getcwd() + "/train_" + \
                         videoFile.split(".")[0] + "_" + str(vol) + ".tfrecords"
-    print("new output path: ", output_path)
+    #print("new output path: ", output_path)
 
     if file_paths:
+        print("file_paths: ", file_paths)
         tfrecord_dataset = tf.data.TFRecordDataset(file_paths)
         parsed_dataset = tfrecord_dataset.map(read_tfrecord_framenumber)
-        for frame_string in parsed_dataset:
-            frames_tagged.add(int(str(frame_string.numpy())[8:-1]))
+        for frame_bytestring in parsed_dataset:
+            frames_tagged.add(int(str(frame_bytestring.numpy(), 'utf-8')))
 
     return frames_tagged, output_path
+
+def order_bounding_box(corner1, corner2):
+    '''
+    Takes as input any two corners of a rectangle and returns two pairs of coordinates -
+    upper left and lower right.
+    '''
+    upper_left = (min(corner1[0], corner2[0]), min(corner1[1], corner2[1]))
+    lower_right = (max(corner1[0], corner2[0]), max(corner1[1], corner2[1]))
+    return [upper_left, lower_right]
+
+def update_dict_coords(output_dict, label, class_text, corner1, corner2):
+    '''
+    '''
+    bb = order_bounding_box(corner1, corner2)
+    output_dict['xmins'].append(bb[0][0])
+    output_dict['ymins'].append(bb[0][1])
+    output_dict['xmaxs'].append(bb[1][0])
+    output_dict['ymaxs'].append(bb[1][1])
+    output_dict['classes'].append(label)
+    output_dict['classes_text'].append(class_text)
+
+def tag_objects(frameId, image, videoFile, write_to_json=False, save_tagged_image=False):
+    output_dict = {'video': str(videoFile), 'source_id': str(int(frameId)), 'image_width':None, 'image_height':None,
+                    'xmins':[], 'xmaxs':[], 'ymins':[], 'ymaxs':[], 'classes':[],
+                    'classes_text':[]}
+    output_dict['image_height']=image.shape[0]
+    output_dict['image_width']=image.shape[1]
+    _, im_buf_arr = cv2.imencode(".jpg", image[:1,:1,:])
+    output_dict['image_encoded']=im_buf_arr.tobytes()
+    clone = image.copy()
+    cv2.namedWindow("image")
+    cv2.startWindowThread()
+    t0= time.clock()
+
+    while True:
+        # display the image and allow bounding box selection
+        cv2.imshow("image", image)
+        ROI = cv2.selectROI("image", image, showCrosshair=False)
+        cv2.destroyWindow("ROI selector")
+        cv2.waitKey(100)
+        corner1 = (ROI[0], ROI[1])
+        corner2 = (ROI[0]+ROI[2], ROI[1]+ROI[3])
+        cv2.rectangle(image, corner1, corner2, (255, 255, 255), 1)
+        cv2.imshow("image", image)
+        key = cv2.waitKey(0) & 0xFF
+
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r"):
+            image = clone.copy()
+
+        # if the 'p' key is pressed, save coordinates, 'person', and draw rectangle in green.
+        if key == ord("p"):
+            image = clone.copy()
+            cv2.rectangle(image, (ROI[0], ROI[1]), (ROI[0]+ROI[2], ROI[1]+ROI[3]), (0, 255, 0), 1)
+            update_dict_coords(output_dict, 1, 'person', corner1, corner2)
+            cv2.imshow('image', image)
+            clone = image.copy()
+
+        # if the 'b' key is pressed, save coordinates, 'bus', and draw rectangle in red.
+        if key == ord("b"):
+            image = clone.copy()
+            cv2.rectangle(image, (ROI[0], ROI[1]), (ROI[0]+ROI[2], ROI[1]+ROI[3]), (0, 0, 255), 1)
+            update_dict_coords(output_dict, 2, 'bus', corner1, corner2)
+            cv2.imshow('image', image)
+            clone = image.copy()
+
+        # if the 'c' key is pressed, save coordinates, 'car', and draw rectangle in yellow.
+        if key == ord("c"):
+            image = clone.copy()
+            cv2.rectangle(image, (ROI[0], ROI[1]), (ROI[0]+ROI[2], ROI[1]+ROI[3]), (0, 255, 255), 1)
+            update_dict_coords(output_dict, 3, 'car', corner1, corner2)
+            cv2.imshow('image', image)
+            clone = image.copy()
+
+        # if the 'q' key is pressed, break from the loop
+        elif key == ord("q"):
+            frame_time = time.clock() - t0
+            break
+
+    #Print tagging time
+
+    #Write output_dict to file
+    if output_dict['classes'] and write_to_json:
+        file_name_json = 'train_data_frame_' + str(int(frameId)) + '.json'
+        with open(json_path + file_name_json, 'w') as fp:
+            json.dump(output_dict, fp)
+
+    #Write tagged image to file
+    if output_dict['classes'] and save_tagged_image:
+        file_name_jpg = 'tagged_image_frame_' + str(int(frameId)) + '.jpg'
+        cv2.imwrite(file_name_jpg,image)
+
+    # close all open windows
+    cv2.destroyAllWindows()
+    cv2.waitKey(10)
+
+    return output_dict, frame_time
+
+
+def update_tracked(output_dict, boxes, clone, next_frameId):
+    new_output_dict = {}
+    new_output_dict['video'] = output_dict['video']
+    new_output_dict['image_width'] = output_dict['image_width']
+    new_output_dict['image_height'] = output_dict['image_height']
+    new_output_dict['classes'] = output_dict['classes']
+    new_output_dict['classes_text'] = output_dict['classes_text']
+
+    updated_xmins = []
+    updated_xmaxs = []
+    updated_ymins = []
+    updated_ymaxs = []
+    for box in boxes:
+        updated_xmins.append(box[0])
+        updated_ymins.append(box[1])
+        updated_xmaxs.append(box[0] + box[2])
+        updated_ymaxs.append(box[1] + box[3])
+    new_output_dict['xmins'] = updated_xmins
+    new_output_dict['ymins'] = updated_ymins
+    new_output_dict['xmaxs'] = updated_xmaxs
+    new_output_dict['ymaxs'] = updated_ymaxs
+    new_output_dict['source_id'] = str(int(next_frameId))
+
+    _, next_im_buf_arr = cv2.imencode(".jpg", clone[:1,:1,:])
+    new_output_dict['image_encoded']=next_im_buf_arr.tobytes()
+
+    return new_output_dict
